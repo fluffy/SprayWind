5e1bb8f6212bf4e940b6d440432435f590d8d45e
Cullen Jennings <fluffy@iii.ca>
2012-12-17 23:20:14 -0600

    update mange to run

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
+
 import os
 import sys
 
+sys.path.append('/home/fluffy/webapps/spraywind')
+sys.path.append('/home/fluffy/webapps/spraywind/SprayWind')
+sys.path.append('/home/fluffy/webapps/spraywind/lib/python2.7')
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
     from django.core.management import execute_from_command_line
 
