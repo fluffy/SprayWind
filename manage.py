9b323aed27f178d8422489e3d255eb7b5cb5e9a5
Cullen Jennings <fluffy@iii.ca>
2012-12-17 23:03:00 -0700

    can run local

--- manage.py
+++ manage.py
@@ -7,6 +7,10 @@ sys.path.append('/home/fluffy/webapps/spraywind')
 sys.path.append('/home/fluffy/webapps/spraywind/SprayWind')
 sys.path.append('/home/fluffy/webapps/spraywind/lib/python2.7')
 
+sys.path.append('/Users/fluffy/src/SprayWind')
+sys.path.append('/usr/local/bin/python2.7')
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
