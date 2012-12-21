25fdbf7a37a54d0a7abc5f3a19f96c43c57aa72a
Cullen Jennings <fluffy@iii.ca>
2012-12-21 09:31:43 -0700

    fixing up setting stuff

--- website/wind/views.py
+++ website/wind/views.py
@@ -6,13 +6,12 @@ from website.wind.models import SensorReading
 
 def getWind( request, sensorName ):
 
-    #now = datetime.datetime.now()
-    #r = SensorReading( sensorID=sensorName, time=now, minWind=1.0, avgWind=2.0, maxWind=3.0 )
-    #r.save()
+    now = datetime.datetime.now()
+    r = SensorReading( sensorID=sensorName, time=now, minWind=1.0, avgWind=2.0, maxWind=3.0 )
+    r.save()
 
     sensorReading = SensorReading.objects.get( sensorID=sensorName )
-    time = sensorReading.time 
-    avgWind = sensorReading.avgWind
 
     return render(request, 'wind.html', 
-    				{ 'time':time, 'avgWind':avgWind } )
+    				{ 'time':sensorReading.time, 
+    				  'avgWind':sensorReading.avgWind } )
