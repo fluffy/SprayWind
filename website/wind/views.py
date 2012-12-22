79e8fe428c2897424dfcf7ff6ed6956db4eefa48
Cullen Jennings <fluffy@iii.ca>
2012-12-22 05:41:30 -0700

    added error handlign to email parse

--- website/wind/views.py
+++ website/wind/views.py
@@ -16,7 +16,7 @@ def getWind( request, sensorName ):
 
 def getInfo( request, sensorName ):
     sensorReading = SensorReading.objects.get( sensorID=sensorName )
-    return HttpResponse( "<html><body>\n" + sensorReading.info + "\n</body></html>" )
+    return HttpResponse( "<html><body><pre>\n" + sensorReading.info + "\n</pre></body></html>" )
 
 
 def cloudMailInJson( request ):
@@ -27,23 +27,26 @@ def cloudMailInJson( request ):
 	log += "request method = %s\n" % request.method 
 	log += "request body = %s \n\n" % request.body 
 
-	email = json.loads( request.body )
-	body = email[ 'plain' ]
-	senml = body[ string.find(body,"{") : string.rfind(body,"}")+1 ]
-	sensor = json.loads( senml )
-
-	log += "senml = %s \n" % json.dumps( sensor ) 
-
 	minWind = -1.0
 	avgWind = -1.0
 	maxWind = -1.0
-	for reading in sensor[ 'e' ]:
-		if reading['n'] == "minWind":  minWind = reading['v']
-		if reading['n'] == "avgWind":  avgWind = reading['v']
-		if reading['n'] == "maxWind":  maxWind = reading['v']
+
+	try:
+		email = json.loads( request.body )
+		body = email[ 'plain' ]
+		senml = body[ string.find(body,"{") : string.rfind(body,"}")+1 ]
+		sensor = json.loads( senml )
+
+		log += "senml = %s \n" % json.dumps( sensor ) 
+
+		for reading in sensor[ 'e' ]:
+			if reading['n'] == "minWind":  minWind = reading['v']
+			if reading['n'] == "avgWind":  avgWind = reading['v']
+			if reading['n'] == "maxWind":  maxWind = reading['v']
+	except Exception as e:
+		log += "Problem parsing the email. Exception %s \n"%e
 
 	log += "wind (min,avg,max) = %s,%s,%s \n"%( minWind, avgWind, maxWind)
-
  	r = SensorReading( sensorID=sensorName, time=now, info=log, minWind=minWind, avgWind=avgWind, maxWind=maxWind )
   	r.save()
 
