6c728c0242675bba64f1505142903bad5c59fd70
Cullen Jennings <fluffy@iii.ca>
2012-12-24 10:26:25 -0700

    put license in files instead of just main

--- Arduino/WindSat/WindSat.ino
+++ Arduino/WindSat/WindSat.ino
@@ -1,15 +1,19 @@
 /* 
- Copyright (c) 2012, Cullen Jennings <fluffy@iii.ca>
- All rights reserved.
+ Copyright (c) 2012, Cullen Jennings <fluffy@iii.ca> All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met: 
  
  1. Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer. 
+ 
  2. Redistributions in binary form must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided with the distribution. 
+ 
+ 3. Neither the name of Cullen Jennings nor the names of its contributors may 
+ be used to endorse or promote products derived from this software without 
+ specific prior written permission.
  
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,::w BUT NOT LIMITED TO, THE IMPLIED
@@ -25,7 +29,7 @@
  
  Example program to connect up SPOT Connect to Arduino using the SparkFun Run SPOT Run board. 
  
- The source code repository for this project is at: TODO 
+ The source code repository for this project is at: https://github.com/fluffy/SprayWind 
  
  Using this Library:
  
